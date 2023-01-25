cac6602f546c905ec33f403131261d8a10e0b2cf
paulego <egopaul@inbox.ru>
2023-01-25 10:01:43 +0500

    Request for groups.

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+import os
+from pprint import pprint
+
 import requests
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 def get_comics() -> dict:
@@ -12,5 +18,22 @@ def get_comics() -> dict:
     return response.json()
 
 
+def get_groups():
+    """
+    Запрос на получение групп.
+    """
+
+    vk_access_token = os.getenv('VK_ACCESS_TOKEN')
+    url = 'https://api.vk.com/method/groups.get'
+    payload = {
+        'access_token': vk_access_token,
+        'v': 5.131
+    }
+    response = requests.get(url, params=payload)
+    response.raise_for_status()
+    return response.json()
+
+
 if __name__ == '__main__':
-    print(get_comics())
+    # print(get_comics()['alt'])
+    pprint(get_groups())
